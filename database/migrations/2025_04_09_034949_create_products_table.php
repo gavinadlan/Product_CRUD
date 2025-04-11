4a771eb23da01aeb9ab473e0db2807f7596aaf85
gavin_adlan <117385972+gavinadlan@users.noreply.github.com>
2025-04-11 13:47:57 +0700

    Update 2025_04_09_034949_create_products_table.php

--- database/migrations/2025_04_09_034949_create_products_table.php
+++ database/migrations/2025_04_09_034949_create_products_table.php
@@ -10,17 +10,16 @@ return new class extends Migration
      * Run the migrations.
      */
     public function up(): void
-{
-    Schema::create('products', function (Blueprint $table) {
-        $table->id();
-        $table->string('name', 100);
-        $table->text('description')->nullable();
-        $table->decimal('price', 10, 2);
-        $table->integer('stock')->default(0);
-        $table->timestamps();
-    });
-}
-
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 100);
+            $table->text('description')->nullable();
+            $table->decimal('price', 10, 2);
+            $table->integer('stock')->default(0);
+            $table->timestamps();
+        });
+    }
 
     /**
      * Reverse the migrations.
